5f59d565768fe36c035fbf37dfa7f38450e6574b
Tinniam V Ganesh <tvganesh.85@gmail.com>
2017-10-03 21:48:18 +0530

    Changes

--- test.R
+++ test.R
@@ -32,4 +32,37 @@ test <- crimesDF[-train_idx, ]
 df1 <- crimesDF %>% select(medIncome,ViolentCrimesPerPop)
 train_idx <- trainTestSplit(df1,trainPercent=75,seed=5)
 train <- df1[train_idx, ]
-test <- df1[-train_idx, ]
+fit <- lm(ViolentCrimesPerPop~medIncome,data=df1)
+summary(fit)
+names(fit)
+plot(df1$medIncome,df1$ViolentCrimesPerPop)
+
+df=read.csv("Boston.csv",stringsAsFactors = FALSE) # Data from MASS - SL
+train_idx <- trainTestSplit(df,trainPercent=75,seed=5)
+train <- df[train_idx, ]
+test <- df[-train_idx, ]
+
+
+fit=lm(medv~lstat,data=df)
+fit
+summary(fit)
+names(fit)
+coef(fit)
+confint(fit)
+
+predict(fit,data.frame(lstat=(c(5,10,15))), interval="confidence")
+predict(fit,data.frame(lstat=(c(5,10,15))), interval="prediction")
+plot(df$lstat,df$medv)
+abline(fit)
+abline(fit,lwd=3)
+abline(fit,lwd=3,col="red")
+
+summary(fit)
+
+pred<-predict(fit,newdata=test)
+actual <- test$medv
+RSS <- sum((actual - pred)^2)
+TSS <- sum((actual - mean(actual))^2)
+rsquared <-1 - (RSS/TSS)
+
+
